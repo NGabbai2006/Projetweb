17d099aadf5f27275bfaf4430988eec4138ea4db
NGabbai2006 <ngabbai.pro@gmail.com>
2026-02-19 16:33:34 +0100

    probleme d'asynchro sur endgame resolu et modif du register pour simplifié et vraiment empecher les doublons

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ connection.connect((err) => {
 
 app.use(express.json());
 app.use(express.static('public')); // servir les fichiers statiques du dossier public
-app.use(express.json()); // middleware pour parser le JSOa
+
 
 
 //=========================================================================================================
@@ -60,24 +60,31 @@ app.get('/Top100', (req, res) => { // route GET pour /scoreTab
 
 // route pour la fin de partie et la mise à jour du score
 app.post('/endgame', (req, res) => { // route POST pour /endgame
-  infoScore(req, res)
 
-  if (res.verifPoints < res.verifPointsTemp) {
-    connection.query('UPDATE Score SET points = pointsTemp , pointsTemp = 0 WHERE idUser = ?',
-      [req.body.id], (err, results) => {
-        if (err) {
-          console.error('Erreur lors de la mise à jour du score :', err);
-          res.status(500).json({ message: 'Erreur serveur' });
-          return;
-        }
-        console.log('score mis à jour av  ec succès');
-        res.json({ message: 'score mis à jour' });
-      });
-  }
-  else
-    (res.json({ message: 'score non changé' }))
+  connection.query('SELECT points, pointsTemp FROM Score WHERE idUser = ?',
+    [req.body.id], (err, results) => {
+      if (err) {
+        console.error('Erreur lors de la récupération du score :', err);
+        res.status(500).json({ message: 'Erreur serveur' });
+
+      }
+
+      if (results[0].points < results[0].pointsTemp) {
+        connection.query('UPDATE Score SET points = pointsTemp , pointsTemp = 0 WHERE idUser = ?',
+          [req.body.id], (err, results) => {
+            if (err) {
+              console.error('Erreur lors de la mise à jour du score :', err);
+              res.status(500).json({ message: 'Erreur serveur' });
+              return;
+            }
+            console.log('score mis à jour av  ec succès');
+            res.json({ message: 'score mis à jour' });
+          });
+      }
+      else
+        (res.json({ message: 'score non changé' }))
+    });
 });
-
 
 // route pour les reponses 
 
@@ -115,57 +122,46 @@ app.post('/reponse', (req, res) => { // route GET pour /reponse
 
 app.post('/register', (req, res) => { // route POST pour /register
   function reloadRegister() {
-    let a = 0; const tokenId = crypto.randomBytes(16).toString('hex');
-    connection.query('SELECT id FROM User',
+    let a = 0;
+    const tokenId = crypto.randomBytes(16).toString('hex'); //genere un tokenid
+    connection.query(
+      'INSERT INTO User (id, login, password) VALUES (?,?, ?)',
+      [tokenId, req.body.V_log, req.body.V_pass],
       (err, results) => {
-        if (err) {
-          console.error('Erreur lors de la récupération des id d\'utilisateurs :', err);
+      if (err) {
+        if(err.code === 'ER_DUP_ENTRY')
+        {
+          console.error('Erreur : token en double, génération d\'un nouveau tokenId');
+          return reloadRegister();
         }
-        //regarde tout les id et compare si le tokenId existe deja ou pas
-        results.forEach(result => {
-          if (tokenId == result.id) {
-            reloadRegister(); //relance la fonction pour générer un nouveau tokenId
-          }
-
-          else if(results.length == 0){
-            a = 1; // si la table user est vide
-          }
-          else {
-            a = 1;
-          }
-        });
-        // si le tokenId est unique alors on insere 
-        if (a == 1) {
+        else{
+          console.error('erreur lors de de l\'inscription :', err);
+          res.status(500).json({ message: 'Erreur serveur lors de l\'inscription' });
+        }
+      }
+        else {
+          // ajout de l'user dans la table score car plus simple 
           connection.query(
-            'INSERT INTO User (id, login, password) VALUES (?,?, ?)',
-            [tokenId, req.body.V_log, req.body.V_pass],
-            (err, results) => {
+            'INSERT INTO Score (idUser) VALUES (?)',
+            [tokenId],
+            (err, resultsScore) => {
               if (err) {
-                console.error('Erreur lors de l\'insertion dans la base de données :', err);
-                res.status(500).json({ message: 'Erreur serveur' });
+                console.error('Erreur lors de l\'insertion dans la table Score :', err);
+                res.status(500).json({ message: 'Erreur serveur lors de l\'insertion du score' });
 
               }
-              // ajout de l'user dans la table score car plus simple 
-              connection.query(
-                'INSERT INTO Score (idUser) VALUES (?)',
-                [tokenId],
-                (err, resultsScore) => {
-                  if (err) {
-                    console.error('Erreur lors de l\'insertion dans la table Score :', err);
-                    res.status(500).json({ message: 'Erreur serveur lors de l\'insertion du score' });
-
-                  }
-                  console.log('Insertion réussie, ID utilisateur :', tokenId);
-                  res.json({ message: 'Inscription réussie !', id: tokenId });
-                }
-              );
-            });
+              console.log('Insertion réussie, ID utilisateur :', tokenId);
+              res.json({ message: 'Inscription réussie !', id: tokenId });
+            }
+          );
         }
-
       });
+
   }
   reloadRegister();
 });
+
+
 
 
 
@@ -207,19 +203,3 @@ app.listen(3000, () => { // démarrage du serveur sur le port 3000
   console.log(`Server running on http://${monIp}:3000`); // log de l'URL du serveur
 })
 
-
-//=========================================================================================================
-// fonction pour récupérer les points et les points temporaires d'un utilisateur
-function infoScore(req, res) {
-  connection.query('SELECT points, pointsTemp FROM Score WHERE idUser = ?',
-    [req.body.id], (err, results) => {
-      if (err) {
-        console.error('Erreur lors de la récupération du score :', err);
-        res.status(500).json({ message: 'Erreur serveur' });
-
-      }
-      const verifPoints = results[0].points;
-      const verifPointsTemp = results[0].pointsTemp;
-    }
-  );
-};
